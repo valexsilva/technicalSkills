d2ca1710cf8bbcae378f7b23dbd35c6c2700e24a
alex Silva <alejandro-valerio.silva@eviden.com>
2024-08-09 14:40:55 -0600

    first commit

---
+++ src/main/java/com/coyote/technicalSkills/TechnicalSkillsApplication.java
@@ -0,0 +1,13 @@
+package com.coyote.technicalSkills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalSkillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalSkillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coyote/technicalSkills/TechnicalSkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coyote.technicalSkills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalSkillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
